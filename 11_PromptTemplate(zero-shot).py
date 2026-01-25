c3d5a4be2ce826dbba4a67c1f4aa3e8799cf768f
Geralt2026 <845326741@qq.com>
2026-01-25 15:26:29 +0800

    PromptTemplate

---
+++ 11_PromptTemplate(zero-shot).py
@@ -0,0 +1,17 @@
+# 提示词优化在模型应用中非常重要，LangChain提供了PromptTemplate类，用来协助优化提示词。
+# PromptTemplate表示提示词模板，可以构建一个自定义的基础提示词模板，支持变量的注入，最终生成所需的提示词。
+
+#标准写法
+from langchain_core.prompts import PromptTemplate
+from langchain_community.llms.tongyi import Tongyi
+
+prompt_template = PromptTemplate.from_template(
+    "我的邻居姓{lastname}, 刚生了{gender}, 帮忙起名字，请简略回答。"
+)
+
+#变量注入，生成提示词文本
+prompt_text = prompt_template.format(lastname="张", gender="女儿")
+
+model = Tongyi(model="qwen-max")
+res = model.invoke(prompt_text)
+print(res)
