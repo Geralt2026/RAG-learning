109afdcb974212fa281ba63f0f2c4027b14ea32d
Geralt2026 <845326741@qq.com>
2026-01-26 22:34:17 +0800

    新增相似度检索阈值配置，优化文档匹配功能

--- LangChain_RAG_Proj/config_data.py
+++ LangChain_RAG_Proj/config_data.py
@@ -9,3 +9,6 @@ chunk_size = 1000
 chunk_overlap = 50
 separators = ["\n\n", "\n", "。", "，", "？", "！", "：", "；", "、", "|", " "]
 max_split_char_number = 1000  # 文本分隔阈值
+
+# 相似度检索阈值
+similarity_threshold = 1 #检索返回匹配文档的数量
