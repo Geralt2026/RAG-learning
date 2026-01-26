6e1bff7beae087545b5b7bbec45bba0a2817511f
Geralt2026 <845326741@qq.com>
2026-01-26 22:34:02 +0800

    完善WEB网页上传服务，添加知识库服务的会话状态管理和加载动画，优化文件内容上传流程

--- LangChain_RAG_Proj/app_file_uploader.py
+++ LangChain_RAG_Proj/app_file_uploader.py
@@ -1,9 +1,12 @@
 """
 基于Streamlit完成WEB网页上传服务
 
+Streamlit: 当web页面元素发生变化, 则代码重新执行一遍
 """
 
+import time
 import streamlit as st
+from knowledge_base import KnowledgeBaseService
 
 # 添加网页标题
 st.title("知识库更新服务")
@@ -15,17 +18,25 @@ uploaded_file = st.file_uploader(
     accept_multiple_files=False,  # 是否允许上传多个文件
 )
 
+# session_state 是一个字典
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBaseService()
+
 if uploaded_file is not None:
     # 提取文件信息
     file_name = uploaded_file.name
     file_type = uploaded_file.type
-    file_size = uploaded_file.size / 1024 # KB
+    file_size = uploaded_file.size / 1024  # KB
 
     st.subheader(f"文件信息：{file_name}")
 
     st.write(f"文件类型：{file_type} | 文件大小：{file_size:.2f} KB")
 
     # 显示文件内容 getvalue() ➡️ bytes ➡️ decode('utf-8')
-    text = uploaded_file.getvalue().decode('utf-8')
-    st.write(text)
-    
+    text = uploaded_file.getvalue().decode("utf-8")
+
+    with st.spinner("载入知识库中..."): # 在spinner内执行代码，会显示一个加载动画
+        time.sleep(1) # 模拟载入时间
+        result = st.session_state["service"].upload_by_str(text, file_name)
+        st.write(result)
+        
